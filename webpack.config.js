ce3b693d2b17eb27e7ddd6aae8f9b247e05aa9c1
car70scep <89488089+car70scep@users.noreply.github.com>
2023-02-25 17:13:52 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = 
+{
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    auth: './src/auth.js',
+    createUser: './src/createUser.js',
+    config: './src/config.js'},
+  
+    output: 
+  {
+    path: path.resolve(__dirname,'dist'),
+    filename: '[name].bundle.js'
+  },
+  
+  watch: true
+}
