1398ff93cc9974ebe8821c6479574e5270c9c077
Hiram Morales <hmorales0312@gmail.com>
2023-03-23 22:36:53 -0600

    added auto scroll functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { initializeApp } from "firebase/app"
+import{ getFirestore, collection, getDocs, addDoc, deleteDoc, doc, updateDoc, query, orderBy, onSnapshot} from "firebase/firestore"
 import { getAuth, onAuthStateChanged,signOut} from 'firebase/auth'
+
+
+
+
 
 
 // Your web app's Firebase configuration
@@ -16,6 +21,67 @@ const firebaseConfig = {
 const fireApp = initializeApp(firebaseConfig);
 const auth = getAuth(fireApp);
 
+//accessing the database
+const db = getFirestore(fireApp);
+const movieCollection = collection(db, 'movie_info')
+
+
+
+
+//making a function to get all the information of the movies from the database
+function gatherMovieInformation(){
+  //get the information from the database and save it to an array
+  var movieData = []
+  getDocs(movieCollection)
+  .then((snapshot) => {
+    
+    snapshot.docs.forEach((doc) => {
+      movieData.push(doc.id) 
+    })
+  })
+    .catch(err => {
+    console.log(err.message)
+  })
+
+  return movieData
+  
+}
+
+
+//create an array to save all the movies data
+var movieData = []
+
+//if the array is empty that means that its the first time entering the page. if so, we wpopulate the array
+if (movieData.length == 0){
+  movieData = gatherMovieInformation()
+}
+
+console.log(movieData);
+console.log(movieData[0]);
+
+
+
+//replace the placeholder information with the informtion from the databse
+// document.getElementById("indexMovie1Details").innerHTML = movieData
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Observer
 onAuthStateChanged(auth, (user) => {
@@ -30,14 +96,14 @@ onAuthStateChanged(auth, (user) => {
   }
 })
 
-const signOutUserForm = document.querySelector("#signOut")
-signOutUserForm.addEventListener("submit", (event)=> {
-  event.preventDefault()
+// const signOutUserForm = document.querySelector("#signOut")
+// signOutUserForm.addEventListener("submit", (event)=> {
+//   event.preventDefault()
 
-  signOut(auth)
-  .then(() => {
-    console.log("Signed out")
-  }).catch((error) => {
+//   signOut(auth)
+//   .then(() => {
+//     console.log("Signed out")
+//   }).catch((error) => {
 
-  })
-})
+//   })
+// })
